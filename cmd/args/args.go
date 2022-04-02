cd4627a26dc1d4d316feb14adb43f69de040e594
Ian Marsman <ian.marsman@gmail.com>
2022-04-01 21:29:24 -0400

    Fixed and using arg for verbose

---
+++ cmd/args/args.go
@@ -0,0 +1,14 @@
+package args
+
+import "github.com/alexflint/go-arg"
+
+func init() {
+	arg.MustParse(&Args)
+}
+
+var Args struct {
+	Bash    bool `arg:"-s,--bash"`
+	CSH     bool `arg:"-c,--csh"`
+	ZSH     bool `arg:"-z,--zsh"`
+	Verbose bool `arg:"-v,--verbose"`
+}
