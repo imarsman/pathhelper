978e270c763cf9cf6aef545a02cfcd3895fc9de0
Ian Marsman <ian.marsman@gmail.com>
2022-04-02 15:49:39 -0400

    Added help output for args

--- cmd/args/args.go
+++ cmd/args/args.go
@@ -6,10 +6,11 @@ func init() {
 	arg.MustParse(&Args)
 }
 
+// Args args used in the app. Public for use in logging package.
 var Args struct {
-	Bash    bool `arg:"-s,--bash"`
-	CSH     bool `arg:"-c,--csh"`
-	ZSH     bool `arg:"-z,--zsh"`
-	Verbose bool `arg:"-v,--verbose"`
-	Init    bool `arg:"-i,--init"`
+	Bash    bool `arg:"-s,--bash" help:"get bash format path settings"`
+	ZSH     bool `arg:"-z,--zsh" help:"get zsh format path settings"`
+	CSH     bool `arg:"-c,--csh" help:"get csh format path settings"`
+	Verbose bool `arg:"-v,--verbose" help:"display issues as paths evaluated"`
+	Init    bool `arg:"-i,--init" help:"check and build user path dirs if necessary"`
 }
