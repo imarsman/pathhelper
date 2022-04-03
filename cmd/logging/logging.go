f273823b925822fd25be480b3ca845b992d91f2c
Ian Marsman <ian.marsman@gmail.com>
2022-04-02 20:30:00 -0400

    Trace added

--- cmd/logging/logging.go
+++ cmd/logging/logging.go
@@ -12,17 +12,22 @@ import (
 // Info a simple logger for printing out info tied to reading files
 var Info *log.Logger
 var Error *log.Logger
+var Trace *log.Logger
 var once sync.Once
 var verbose bool
 
 func init() {
 	once.Do(func() {
+		Trace = log.New(io.Discard, "TRACE ", log.LUTC)
+		Info = log.New(io.Discard, "INFO ", log.LUTC)
+		Error = log.New(io.Discard, "ERROR ", log.LUTC)
+
 		if args.Args.Verbose {
 			Info = log.New(os.Stderr, "INFO ", log.LUTC)
 			Error = log.New(os.Stderr, "ERROR ", log.LUTC)
-		} else {
-			Info = log.New(io.Discard, "INFO ", log.LUTC)
-			Error = log.New(io.Discard, "ERROR ", log.LUTC)
+		}
+		if args.Args.Trace {
+			Trace = log.New(os.Stderr, "TRACE ", log.LUTC)
 		}
 	})
 }
