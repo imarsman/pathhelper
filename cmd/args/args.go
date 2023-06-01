caaac273ef657ea4bccb73ee0f8c3ca3aa4511ff
Ian Marsman <ian.marsman@gmail.com>
2023-05-31 21:52:25 -0400

    Got rid of flag arg.

--- cmd/args/args.go
+++ cmd/args/args.go
@@ -23,12 +23,11 @@ func init() {
 
 // Args args used in the app. Public for use in logging package.
 var Args struct {
-	Bash   bool `arg:"-s,--bash" help:"get bash format path settings"`
-	ZSH    bool `arg:"-z,--zsh" help:"get zsh format path settings"`
-	CSH    bool `arg:"-c,--csh" help:"get csh format path settings"`
-	Verify bool `arg:"-V,--verify" help:"verify paths' existence before adding"`
-	Trace  bool `arg:"-t,--trace" help:"show paths evaluated and time do evaluate"`
-	// Init      bool `arg:"-i,--init" help:"check and build user path dirs if necessary"`
+	Bash      bool `arg:"-s,--bash" help:"get bash format path settings"`
+	ZSH       bool `arg:"-z,--zsh" help:"get zsh format path settings"`
+	CSH       bool `arg:"-c,--csh" help:"get csh format path settings"`
+	Verify    bool `arg:"-V,--verify" help:"verify paths' existence before adding"`
+	Trace     bool `arg:"-t,--trace" help:"show paths evaluated and time do evaluate"`
 	Verbose   bool `arg:"-v,--verbose" help:"display issues as paths evaluated"`
 	UserFirst bool `arg:"-u,--user-first" help:"put user directories first"`
 }
